9cca747a58e9c3f1861b9c2b47b5b35db4e8ef67
Bektur Razzakov <brzzkv@gmail.com>
2024-10-09 21:25:03 +0600

    logout when access token expires

--- src/axiosApi.js
+++ src/axiosApi.js
@@ -1,17 +1,27 @@
 import axios from 'axios';
 import { apiUrl } from './constants';
+import { logout } from "./features/user/userThunk";
 
 export const addInterceptors = (store) => {
+  const { dispatch } = store;
+  
   axiosApi.interceptors.request.use((config) => {
     const { url } = config;
     const isSignIn = url?.includes('/token');
     if (!isSignIn) {
-      const token = store.getState().userState.user.access;
+      const { access } = store.getState().userState.user;
       const headers = config.headers;
-      console.log(token);
-      headers.set('Authorization', `Bearer ${token}`);
+      headers.set('Authorization', `Bearer ${access}`);
     }
     return config;
+  });
+  
+  axiosApi.interceptors.response.use(function (response) {
+    return response;
+  }, function (error) {
+    if (error.status === 401 && error.response.data.code === 'token_not_valid') {
+      dispatch(logout());
+    }
   });
 };
 
