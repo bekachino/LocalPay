638495073cafe5d1481f05faf3b4b80da362e730
Bektur Razzakov <brzzkv@gmail.com>
2024-10-09 16:44:33 +0600

    Select component

---
+++ src/Components/Select/Select.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Select = ({
+  color = 'primary',
+  size = 'medium',
+  className,
+  ...rest
+}) => {
+  return (
+    <select
+      className={`${className} custom-input custom-input-color-${![
+        'primary',
+        'secondary',
+        'success',
+        'error'
+      ].includes(color) ? 'primary' : color} custom-input-size-${![
+        'small',
+        'medium',
+        'large'
+      ].includes(size) ? 'medium' : size}`}
+      {...rest}
+    ></select>
+  );
+};
+
+export default Select;
