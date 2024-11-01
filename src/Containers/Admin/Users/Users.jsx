f1723ba9dab84c8b8006a2d99ccafdbb4b78a772
Bektur Razzakov <brzzkv@gmail.com>
2024-11-01 12:14:12 +0600

    sort users by regions

--- src/Containers/Admin/Users/Users.jsx
+++ src/Containers/Admin/Users/Users.jsx
@@ -7,7 +7,8 @@ import IconButton from '../../../Components/UI/IconButton/IconButton';
 import SmallEditIcon from '../../../assets/small-edit-icon.svg';
 import SmallDeleteIcon from '../../../assets/small-delete-icon.svg';
 import { ROLES } from '../../../constants';
-import UserDeleteConfirmation from '../../../Components/UI/UserDeleteConfirmation/UserDeleteConfirmation';
+import UserDeleteConfirmation
+  from '../../../Components/UI/UserDeleteConfirmation/UserDeleteConfirmation';
 import Select from '../../../Components/UI/Select/Select';
 import { jwtDecode } from 'jwt-decode';
 import { useAppSelector } from '../../../app/hooks';
@@ -18,9 +19,11 @@ import './users.css';
 
 const Users = () => {
   const dispatch = useDispatch();
-  const { users, usersLoading, usersPagesAmount } = useSelector(
-    (state) => state.adminState
-  );
+  const {
+    users,
+    usersLoading,
+    usersPagesAmount,
+  } = useSelector((state) => state.adminState);
   const { user } = useAppSelector((state) => state.userState);
   const [usersInDeleteProcess, setUsersInDeleteProcess] = useState([]);
   const [deleteUserId, setDeleteUserId] = useState('');
@@ -32,41 +35,54 @@ const Users = () => {
   const { role } = jwtDecode(user.access || '');
   const [chosenUsers, setChosenUsers] = useState([]);
   const [listAction, setListAction] = useState('');
-
+  const [sortedUsers, setSortedUsers] = useState([]);
+  const [sortByRegion, setSortByRegion] = useState(0);
+  
   useEffect(() => {
-    dispatch(
-      getUsers({
-        ...paginationData,
-        searchWord,
-      })
-    );
+    dispatch(getUsers({
+      ...paginationData,
+      searchWord,
+    }));
   }, [
     // do not add searchWord, dateFilter as deps
     dispatch,
     paginationData,
   ]);
-
+  
+  useEffect(() => {
+    if (sortByRegion === 0) return setSortedUsers([...users]);
+    setSortedUsers([...users].sort((a, b) => (
+      sortByRegion === 1 ? a : b
+    ).region.localeCompare((
+      sortByRegion === 1 ? b : a
+    ).region)));
+  }, [
+    sortByRegion,
+    users,
+  ]);
+  
   const handleSearchWordChange = (e) => {
     setSearchWord(e.target.value);
   };
-
+  
   const searchWithFilters = () => {
-    dispatch(
-      getUsers({
-        ...paginationData,
-        searchWord,
-      })
-    );
+    dispatch(getUsers({
+      ...paginationData,
+      searchWord,
+    }));
   };
-
+  
   const onListActionChange = (e) => {
     const { value } = e.target;
     setListAction(value || '');
   };
-
+  
   const onDeleteUser = async (id) => {
     if (id) {
-      setUsersInDeleteProcess((prevState) => [...prevState, id]);
+      setUsersInDeleteProcess((prevState) => [
+        ...prevState,
+        id,
+      ]);
       await dispatch(deleteUser(id));
       setUsersInDeleteProcess((prevState) => [
         ...prevState.filter((prevId) => prevId !== id),
@@ -75,82 +91,81 @@ const Users = () => {
       dispatch(getUsers({ ...paginationData }));
     }
   };
-
+  
   const toggleDeleteUserModal = (userId) => {
     setDeleteUserId(userId || '');
   };
-
+  
   const onPaginationDataChange = (e) => {
-    const { name, value } = e.target;
-    setPaginationData((prevState) => ({
-      ...prevState,
-      [name]: value,
-    }));
+    const {
+      name,
+      value,
+    } = e.target;
+    setPaginationData((prevState) => (
+      {
+        ...prevState,
+        [name]: value,
+      }
+    ));
   };
-
+  
   const onChooseUser = (e, id) => {
     const { checked } = e.target;
-
+    
     if (id === 'all') {
-      if (chosenUsers?.length === users?.length) setChosenUsers(() => []);
-      else setChosenUsers(() => [...users?.map((user) => user?.id)]);
+      if (chosenUsers?.length === users?.length) setChosenUsers(() => []); else setChosenUsers(() => [...users?.map((user) => user?.id)]);
       return;
     }
-
+    
     if (checked) {
       if (chosenUsers.includes(id)) return;
-      setChosenUsers([...chosenUsers, id]);
+      setChosenUsers([
+        ...chosenUsers,
+        id,
+      ]);
     } else {
       const filteredList = [...chosenUsers].filter((user) => user !== id);
       setChosenUsers(filteredList);
     }
   };
-
+  
   const handleExcelFileExport = (data) => {
     const workbook = XLSX.utils.book_new();
-
+    
     const worksheet = XLSX.utils.json_to_sheet([]);
-
+    
     let rowIndex = 1;
-
-    XLSX.utils.sheet_add_aoa(
-      worksheet,
+    
+    XLSX.utils.sheet_add_aoa(worksheet, [
       [
-        [
-          'ФИО',
-          'Баланс',
-          'Регион',
-          'Дата регистрации',
-          'Роль',
-          'Статус активности',
-          'Планап id',
-        ],
+        'ФИО',
+        'Баланс',
+        'Регион',
+        'Дата регистрации',
+        'Роль',
+        'Статус активности',
+        'Планап id',
       ],
-      { origin: `A1` }
-    );
-
+    ], { origin: `A1` });
+    
     data.forEach((payment) => {
       if (!chosenUsers.includes(payment.id)) return;
-
+      
       rowIndex += 1;
-
-      XLSX.utils.sheet_add_aoa(
-        worksheet,
+      
+      XLSX.utils.sheet_add_aoa(worksheet, [
         [
-          [
-            `${payment.name || '-'} ${payment.surname || '-'}`,
-            payment.balance || '-',
-            payment.region || '-',
-            !!payment?.date_reg ? formatDate(payment.date_reg) : '-',
-            payment.role || '-',
-            payment.is_active || '-',
-            payment.planup_id || '-',
-          ],
+          `${payment.name || '-'} ${payment.surname || '-'}`,
+          payment.balance || '-',
+          payment.region || '-',
+          !!payment?.date_reg ? formatDate(payment.date_reg) : '-',
+          payment.role || '-',
+          payment.is_active || '-',
+          payment.planup_id || '-',
         ],
-        { origin: `A${rowIndex}` }
-      );
+      ], { origin: `A${rowIndex}` });
     });
-
+    
     worksheet['!cols'] = [
       { wch: 25 },
       { wch: 12 },
@@ -158,29 +173,31 @@ const Users = () => {
       { wch: 15 },
       { wch: 10 },
     ];
-
+    
     XLSX.utils.book_append_sheet(workbook, worksheet, 'Пользователи');
-
+    
     XLSX.writeFile(workbook, 'Пользователи.xlsx');
   };
-
+  
   const onActionExecute = () => {
-    if (listAction === 'uploadChosenOptions' && chosenUsers.length)
-      handleExcelFileExport(users);
+    if (listAction === 'uploadChosenOptions' && chosenUsers.length) handleExcelFileExport(users);
   };
-
+  
   return (
-    <div className="users">
-      <Paper className="home-paper" style={{ maxWidth: '1120px' }}>
+    <div className='users'>
+      <Paper
+        className='home-paper'
+        style={{ maxWidth: '1120px' }}
+      >
         <h1>{usersLoading ? 'Загрузка...' : 'Пользователи'}</h1>
-        <div className="user-filters">
+        <div className='user-filters'>
           <Input
-            size="small"
-            placeholder="поиск..."
+            size='small'
+            placeholder='поиск...'
             onChange={handleSearchWordChange}
           />
           <CustomButton
-            size="small"
+            size='small'
             rounded
             onClick={searchWithFilters}
             loading={usersLoading}
@@ -188,122 +205,130 @@ const Users = () => {
             Искать...
           </CustomButton>
         </div>
-        <div className="users-list-container">
-          <table className="users-list">
+        <div className='users-list-container'>
+          <table className='users-list'>
             <thead>
-              <tr>
-                <th>
-                  <input
-                    type="checkbox"
-                    checked={chosenUsers?.length === users?.length}
-                    onChange={(e) => onChooseUser(e, 'all')}
-                  />
-                </th>
-                <th>№</th>
-                <th>ФИО</th>
-                <th>Логин</th>
-                <th>Область</th>
-                <th>Доступный баланс</th>
-                <th>Затраты</th>
-                <th>Списания</th>
-                <th>Пополнения</th>
-                <th>Дата регистрации</th>
-                <th>Комментарий</th>
-                <th>Роль</th>
-                <th>Статус</th>
-                {['admin'].includes(role) && <th>Действия</th>}
-              </tr>
+            <tr>
+              <th>
+                <input
+                  type='checkbox'
+                  checked={chosenUsers?.length === users?.length}
+                  onChange={(e) => onChooseUser(e, 'all')}
+                />
+              </th>
+              <th>№</th>
+              <th>ФИО</th>
+              <th>Логин</th>
+              <th
+                style={{cursor: 'pointer'}}
+                onClick={() => setSortByRegion(sortByRegion === 0 ? 1 : sortByRegion === 1 ? 2 : 0)}
+              >
+                Область {sortByRegion === 1 ? '▼' : sortByRegion === 2 ? '▲' : '' }
+              </th>
+              <th>Доступный баланс</th>
+              <th>Затраты</th>
+              <th>Списания</th>
+              <th>Пополнения</th>
+              <th>Дата регистрации</th>
+              <th>Комментарий</th>
+              <th>Роль</th>
+              <th>Статус</th>
+              {['admin'].includes(role) && <th>Действия</th>}
+            </tr>
             </thead>
             <tbody>
-              {users?.map((user) => (
-                <tr key={user.id}>
-                  <th>
-                    <input
-                      type="checkbox"
-                      onChange={(e) => onChooseUser(e, user?.id)}
-                      checked={chosenUsers.includes(user?.id)}
-                    />
-                  </th>
-                  <td>{user.id}</td>
-                  <td>
-                    {user.name || '-'} {user.surname || '-'}
-                  </td>
-                  <td>{user.login || '-'}</td>
-                  <td>{user.region || '-'}</td>
-                  <td style={{ textAlign: 'center' }}>
-                    {user.balance || 0}
-                    <span className="currence-highlight">с</span>
-                  </td>
-                  <td style={{ textAlign: 'center' }}>
-                    {user.avail_balance || 0}
-                    <span className="currence-highlight">с</span>
-                  </td>
-                  <td style={{ textAlign: 'center' }}>
-                    {user.write_off || 0}
-                    <span className="currence-highlight">с</span>
-                  </td>
-                  <td style={{ textAlign: 'center' }}>
-                    {user.refill || 0}
-                    <span className="currence-highlight">с</span>
-                  </td>
-                  <td>{!!user.date_reg ? formatDate(user.date_reg) : '-'}</td>
-                  <td style={{ textAlign: !!user.comment ? 'left' : 'center' }}>
-                    {user.comment || '-'}
-                  </td>
-                  <td style={{ textAlign: 'center' }}>
-                    {ROLES.find((role) => role.en === user?.role)?.ru || '-'}
-                  </td>
+            {sortedUsers?.map((user) => (
+              <tr key={user.id}>
+                <th>
+                  <input
+                    type='checkbox'
+                    onChange={(e) => onChooseUser(e, user?.id)}
+                    checked={chosenUsers.includes(user?.id)}
+                  />
+                </th>
+                <td>{user.id}</td>
+                <td>
+                  {user.name || '-'} {user.surname || '-'}
+                </td>
+                <td>{user.login || '-'}</td>
+                <td>{user.region || '-'}</td>
+                <td style={{ textAlign: 'center' }}>
+                  {user.balance || 0}
+                  <span className='currence-highlight'>с</span>
+                </td>
+                <td style={{ textAlign: 'center' }}>
+                  {user.avail_balance || 0}
+                  <span className='currence-highlight'>с</span>
+                </td>
+                <td style={{ textAlign: 'center' }}>
+                  {user.write_off || 0}
+                  <span className='currence-highlight'>с</span>
+                </td>
+                <td style={{ textAlign: 'center' }}>
+                  {user.refill || 0}
+                  <span className='currence-highlight'>с</span>
+                </td>
+                <td>{!!user.date_reg ? formatDate(user.date_reg) : '-'}</td>
+                <td style={{ textAlign: !!user.comment ? 'left' : 'center' }}>
+                  {user.comment || '-'}
+                </td>
+                <td style={{ textAlign: 'center' }}>
+                  {ROLES.find((role) => role.en === user?.role)?.ru || '-'}
+                </td>
+                <td
+                  className={`user-status-${user.is_active ? 'active' : 'inactive'}`}
+                >
+                  {user.is_active ? 'активный' : 'заблокирован'}
+                </td>
+                {['admin'].includes(role) && (
                   <td
-                    className={`user-status-${user.is_active ? 'active' : 'inactive'}`}
+                    style={{
+                      position: 'relative',
+                      padding: '0',
+                    }}
                   >
-                    {user.is_active ? 'активный' : 'заблокирован'}
+                    <div className='user-action-btns'>
+                      <IconButton
+                        icon={SmallEditIcon}
+                        color='success'
+                        size='20px'
+                        linkTo={`/edit-user/${user?.id}`}
+                      />
+                      <IconButton
+                        icon={SmallDeleteIcon}
+                        color='error'
+                        size='20px'
+                        onClick={() => toggleDeleteUserModal(user?.id)}
+                        loading={usersInDeleteProcess.includes(user?.id)}
+                      />
+                    </div>
                   </td>
-                  {['admin'].includes(role) && (
-                    <td
-                      style={{
-                        position: 'relative',
-                        padding: '0',
-                      }}
-                    >
-                      <div className="user-action-btns">
-                        <IconButton
-                          icon={SmallEditIcon}
-                          color="success"
-                          size="20px"
-                          linkTo={`/edit-user/${user?.id}`}
-                        />
-                        <IconButton
-                          icon={SmallDeleteIcon}
-                          color="error"
-                          size="20px"
-                          onClick={() => toggleDeleteUserModal(user?.id)}
-                          loading={usersInDeleteProcess.includes(user?.id)}
-                        />
-                      </div>
-                    </td>
-                  )}
-                </tr>
-              ))}
+                )}
+              </tr>
+            ))}
             </tbody>
           </table>
-          <div className="pagination-container">
-            <div className="list-actions">
+          <div className='pagination-container'>
+            <div className='list-actions'>
               <div
-                className="pagination-field-wrapper"
+                className='pagination-field-wrapper'
                 style={{ marginLeft: 'auto' }}
               >
-                <span className="pagination-field-title">
+                <span className='pagination-field-title'>
                   Выберите действие:
                 </span>
-                <Select size="small" onChange={onListActionChange}>
-                  <option value="">-</option>
-                  <option value="uploadChosenOptions">
+                <Select
+                  size='small'
+                  onChange={onListActionChange}
+                >
+                  <option value=''>-</option>
+                  <option value='uploadChosenOptions'>
                     Выгрузить выбранные
                   </option>
                 </Select>
               </div>
               <CustomButton
-                size="small"
+                size='small'
                 style={{ marginTop: 'auto' }}
                 rounded
                 onClick={onActionExecute}
@@ -312,35 +337,38 @@ const Users = () => {
               </CustomButton>
             </div>
             <div
-              className="pagination-field-wrapper"
+              className='pagination-field-wrapper'
               style={{ marginLeft: 'auto' }}
             >
-              <span className="pagination-field-title">
+              <span className='pagination-field-title'>
                 Пользователей на страницу:
               </span>
               <Select
-                size="small"
-                name="page_size"
+                size='small'
+                name='page_size'
                 value={paginationData.page_size}
                 onChange={onPaginationDataChange}
               >
-                <option value="5">5</option>
-                <option value="10">10</option>
-                <option value="20">20</option>
-                <option value="50">50</option>
-                <option value="100">100</option>
+                <option value='5'>5</option>
+                <option value='10'>10</option>
+                <option value='20'>20</option>
+                <option value='50'>50</option>
+                <option value='100'>100</option>
               </Select>
             </div>
-            <div className="pagination-field-wrapper">
-              <span className="pagination-field-title">Страница:</span>
+            <div className='pagination-field-wrapper'>
+              <span className='pagination-field-title'>Страница:</span>
               <Select
-                size="small"
-                name="page"
+                size='small'
+                name='page'
                 value={paginationData.page}
                 onChange={onPaginationDataChange}
               >
                 {Array.from({ length: usersPagesAmount || 0 }, (_, index) => (
-                  <option key={index + 1} value={index + 1}>
+                  <option
+                    key={index + 1}
+                    value={index + 1}
+                  >
                     {index + 1}
                   </option>
                 ))}
--- src/utils.js
+++ src/utils.js
@@ -3,23 +3,23 @@ import * as XLSX from 'xlsx-js-style';
 export const formatDate = (date) => {
   const newDate = new Date(date);
   const pad = (num, size) => num.toString().padStart(size, '0');
-
+  
   const year = newDate.getFullYear();
   const month = pad(newDate.getMonth() + 1, 2);
   const day = pad(newDate.getDate(), 2);
   const hours = pad(newDate.getHours(), 2);
   const minutes = pad(newDate.getMinutes(), 2);
-
+  
   return `${day}.${month}.${year}  ${!!hours && `${hours}:${minutes}`}`;
 };
 
 export const handleExcelFileExport = (payments, chosenPayments) => {
   const workbook = XLSX.utils.book_new();
-
+  
   const worksheet = XLSX.utils.json_to_sheet([]);
-
+  
   let rowIndex = 1;
-
+  
   XLSX.utils.sheet_add_aoa(
     worksheet,
     [
@@ -32,14 +32,14 @@ export const handleExcelFileExport = (payments, chosenPayments) => {
         'Сервис инженер',
       ],
     ],
-    { origin: `A1` }
+    { origin: `A1` },
   );
-
+  
   payments.forEach((payment) => {
     if (!chosenPayments.includes(payment.number_payment)) return;
-
+    
     rowIndex += 1;
-
+    
     XLSX.utils.sheet_add_aoa(
       worksheet,
       [
@@ -52,11 +52,12 @@ export const handleExcelFileExport = (payments, chosenPayments) => {
           payment.user_name || '-',
         ],
       ],
-      { origin: `A${rowIndex}` }
+      { origin: `A${rowIndex}` },
     );
   });
-
+  
   worksheet['!cols'] = [
+    { wch: 25 },
     { wch: 25 },
     { wch: 25 },
     { wch: 17 },
@@ -64,27 +65,27 @@ export const handleExcelFileExport = (payments, chosenPayments) => {
     { wch: 20 },
     { wch: 25 },
   ];
-
+  
   XLSX.utils.book_append_sheet(workbook, worksheet, 'Платежи');
-
+  
   XLSX.writeFile(workbook, 'Платежи.xlsx');
 };
 
 export const handleNewVersionExcelFileExport = (payments) => {
   let totalLocalPaySum = 0;
   let totalPlanUpSum = 0;
-
+  
   const workbook = XLSX.utils.book_new();
   const worksheet = XLSX.utils.json_to_sheet([]);
-
+  
   let rowIndex = 1;
-
+  
   [...payments?.slice(0, payments?.length - 1)]?.forEach((payment) => {
     const userName = `Платежи пользователя: ${payment?.name || '-'} ${payment?.surname || '-'}`;
     XLSX.utils.sheet_add_aoa(worksheet, [[userName]], {
       origin: `A${rowIndex}`,
     });
-
+    
     worksheet['!merges'] = worksheet['!merges'] || [];
     worksheet['!merges'].push({
       s: {
@@ -96,7 +97,7 @@ export const handleNewVersionExcelFileExport = (payments) => {
         c: 5,
       },
     });
-
+    
     const cellAddress = XLSX.utils.encode_cell({
       r: rowIndex - 1,
       c: 0,
@@ -110,31 +111,33 @@ export const handleNewVersionExcelFileExport = (payments) => {
         bold: true,
       },
     };
-
+    
     rowIndex += 1;
-
+    
     XLSX.utils.sheet_add_aoa(
       worksheet,
       [
         [
           'Время проведения платежа',
+          'Время платежа PlanUp',
           'Лицевой счет',
           'Баланс в ЛокалПэй',
           'Баланс в Планап',
           'Статус платежа',
         ],
       ],
-      { origin: `A${rowIndex}` }
+      { origin: `A${rowIndex}` },
     );
-
+    
     rowIndex += 1;
-
+    
     payment?.payments.forEach((singleUserPayment) => {
       XLSX.utils.sheet_add_aoa(
         worksheet,
         [
           [
             singleUserPayment.date_payment || '-',
+            singleUserPayment.end_date || '-',
             singleUserPayment.ls_abon || '-',
             singleUserPayment.localpay_money || 0,
             singleUserPayment.planup_money || 0,
@@ -144,33 +147,37 @@ export const handleNewVersionExcelFileExport = (payments) => {
               : '-',
           ],
         ],
-        { origin: `A${rowIndex}` }
+        { origin: `A${rowIndex}` },
       );
       rowIndex += 1;
     });
-
+    
     const sumLocalPayMoney = payment?.payments.reduce((total, payment) => {
       return (
         total +
-        (Number.isInteger(Number(payment?.localpay_money)) &&
-        payment?.status_payment === 'Выполнен'
-          ? Number(payment?.localpay_money)
-          : 0)
+        (
+          Number.isInteger(Number(payment?.localpay_money)) &&
+          payment?.status_payment === 'Выполнен'
+            ? Number(payment?.localpay_money)
+            : 0
+        )
       );
     }, 0);
     const sumPlanUpMoney = payment?.payments.reduce((total, payment) => {
       return (
         total +
-        (Number.isInteger(Number(payment?.planup_money)) &&
-        payment?.status_payment === 'Выполнен'
-          ? Number(payment?.planup_money)
-          : 0)
+        (
+          Number.isInteger(Number(payment?.planup_money)) &&
+          payment?.status_payment === 'Выполнен'
+            ? Number(payment?.planup_money)
+            : 0
+        )
       );
     }, 0);
-
+    
     totalLocalPaySum += sumLocalPayMoney;
     totalPlanUpSum += sumPlanUpMoney;
-
+    
     XLSX.utils.sheet_add_aoa(
       worksheet,
       [
@@ -183,14 +190,14 @@ export const handleNewVersionExcelFileExport = (payments) => {
           '',
         ],
       ],
-      { origin: `A${rowIndex}` }
+      { origin: `A${rowIndex}` },
     );
-
+    
     rowIndex += 2;
   });
-
+  
   rowIndex += 2;
-
+  
   XLSX.utils.sheet_add_aoa(
     worksheet,
     [
@@ -203,9 +210,9 @@ export const handleNewVersionExcelFileExport = (payments) => {
         '',
       ],
     ],
-    { origin: `A${rowIndex}` }
+    { origin: `A${rowIndex}` },
   );
-
+  
   const localPaySumCell = XLSX.utils.encode_cell({
     r: rowIndex - 1,
     c: 2,
@@ -214,7 +221,7 @@ export const handleNewVersionExcelFileExport = (payments) => {
     r: rowIndex - 1,
     c: 3,
   });
-
+  
   worksheet[localPaySumCell].s = {
     alignment: {
       horizontal: 'center',
@@ -233,8 +240,9 @@ export const handleNewVersionExcelFileExport = (payments) => {
       bold: true,
     },
   };
-
+  
   worksheet['!cols'] = [
+    { wch: 35 },
     { wch: 35 },
     { wch: 17 },
     { wch: 23 },
@@ -242,8 +250,8 @@ export const handleNewVersionExcelFileExport = (payments) => {
     { wch: 20 },
     { wch: 25 },
   ];
-
+  
   XLSX.utils.book_append_sheet(workbook, worksheet, 'Платежи по пользователям');
-
+  
   XLSX.writeFile(workbook, 'Платежи по пользователям.xlsx');
 };
