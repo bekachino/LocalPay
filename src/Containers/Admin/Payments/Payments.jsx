24089a57c5fff0c9b21f7d07cb88fbf6563deafe
Bektur Razzakov <brzzkv@gmail.com>
2024-11-12 18:07:26 +0600

    prettier

--- src/Containers/Admin/Payments/Payments.jsx
+++ src/Containers/Admin/Payments/Payments.jsx
@@ -4,7 +4,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import {
   annulPayment,
   getPayments,
-  getPaymentsForUpload, getUsers,
+  getPaymentsForUpload,
+  getUsers,
 } from '../../../features/admin/adminThunk';
 import {
   formatDate,
@@ -41,20 +42,18 @@ const Payments = () => {
   const [chosenPayments, setChosenPayments] = useState([]);
   const [listAction, setListAction] = useState('');
   const [paymentInAnnulmentProcess, setPaymentInAnnulmentProcess] = useState(
-    [],
+    []
   );
-  
+
   useEffect(() => {
     dispatch(
       getUsers({
         page: 1,
         page_size: 99999,
-      }),
+      })
     );
-  }, [
-    dispatch,
-  ]);
-  
+  }, [dispatch]);
+
   useEffect(() => {
     if (!paymentsPagesAmount || paginationData.page < paymentsPagesAmount) {
       dispatch(
@@ -62,7 +61,7 @@ const Payments = () => {
           ...paginationData,
           searchWord,
           ...dateFilter,
-        }),
+        })
       );
     }
   }, [
@@ -70,38 +69,31 @@ const Payments = () => {
     dispatch,
     paginationData,
   ]);
-  
+
   const handleSearchWordChange = (e) => {
     setSearchWord(e.target.value);
   };
-  
+
   useEffect(() => {
     setSearchWord(searchWord?.trim());
   }, [searchWord]);
-  
+
   const handleDateFilterChange = (e) => {
-    const {
-      name,
-      value,
-    } = e.target;
-    
-    setDateFilter((prevState) => (
-      {
-        ...prevState,
-        [name]: value,
-      }
-    ));
+    const { name, value } = e.target;
+
+    setDateFilter((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
   };
-  
+
   const onShowMore = async () => {
-    setPaginationData((prevState) => (
-      {
-        ...prevState,
-        page: prevState.page + 1,
-      }
-    ));
+    setPaginationData((prevState) => ({
+      ...prevState,
+      page: prevState.page + 1,
+    }));
   };
-  
+
   const searchWithFilters = () => {
     dispatch(
       getPayments({
@@ -109,13 +101,13 @@ const Payments = () => {
         searchWord,
         ...dateFilter,
         isSearch: true,
-      }),
+      })
     );
   };
-  
+
   const onChoosePayment = (e, id) => {
     const { checked } = e.target;
-    
+
     if (id === 'all') {
       if (chosenPayments?.length === payments?.length)
         setChosenPayments(() => []);
@@ -125,30 +117,27 @@ const Payments = () => {
         ]);
       return;
     }
-    
+
     if (checked) {
       if (chosenPayments.includes(id)) return;
-      setChosenPayments([
-        ...chosenPayments,
-        id,
-      ]);
+      setChosenPayments([...chosenPayments, id]);
     } else {
       const filteredList = [...chosenPayments].filter(
-        (payment) => payment !== id,
+        (payment) => payment !== id
       );
       setChosenPayments(filteredList);
     }
   };
-  
+
   const onListActionChange = (e) => {
     const { value } = e.target;
     setListAction(value || '');
   };
-  
+
   const onActionExecute = () => {
     if (listAction === 'uploadChosenOptions' && chosenPayments.length)
       handleExcelFileExport(payments, chosenPayments);
-    
+
     if (
       listAction === 'newVersionUpload' &&
       !!dateFilter?.date_from &&
@@ -157,20 +146,23 @@ const Payments = () => {
       dispatch(
         getPaymentsForUpload({
           ...dateFilter,
-          user_id: users?.find((payment) => (
-            payment?.name?.toLowerCase() || ''
-          ).includes(searchWord?.toLowerCase() || '') || (
-            payment?.surname?.toLowerCase() || ''
-          ).includes(searchWord?.toLowerCase() || ''))
-            ?.id,
-        }),
+          user_id: users?.find(
+            (payment) =>
+              (payment?.name?.toLowerCase() || '').includes(
+                searchWord?.toLowerCase() || ''
+              ) ||
+              (payment?.surname?.toLowerCase() || '').includes(
+                searchWord?.toLowerCase() || ''
+              )
+          )?.id,
+        })
       ).then((res) => {
         if (res?.error) {
           dispatch(
             addAlert({
               type: 'warning',
               message: 'Данные отсуствуют',
-            }),
+            })
           );
           return;
         }
@@ -178,13 +170,10 @@ const Payments = () => {
       });
     }
   };
-  
+
   const onPaymentAnnulment = async (id) => {
     if (id) {
-      setPaymentInAnnulmentProcess((prevState) => [
-        ...prevState,
-        id,
-      ]);
+      setPaymentInAnnulmentProcess((prevState) => [...prevState, id]);
       await dispatch(annulPayment(id));
       setPaymentInAnnulmentProcess((prevState) => [
         ...prevState.filter((prevId) => prevId !== id),
@@ -194,43 +183,43 @@ const Payments = () => {
           ...paginationData,
           searchWord,
           ...dateFilter,
-        }),
+        })
       );
     }
   };
-  
+
   return (
-    <div className='payments'>
-      <Paper className='home-paper'>
+    <div className="payments">
+      <Paper className="home-paper">
         <h1>{paymentsLoading || usersLoading ? 'Загрузка...' : 'Платежи'}</h1>
-        <div className='user-filters'>
-          <div className='user-filters-inner'>
+        <div className="user-filters">
+          <div className="user-filters-inner">
             <Input
-              size='small'
-              placeholder='поиск...'
-              color='success'
+              size="small"
+              placeholder="поиск..."
+              color="success"
               onChange={handleSearchWordChange}
             />
             <Input
-              type='date'
-              name='date_from'
+              type="date"
+              name="date_from"
               value={dateFilter?.date_from}
-              color='success'
-              size='small'
+              color="success"
+              size="small"
               onChange={handleDateFilterChange}
             />
             <Input
-              type='date'
-              name='date_to'
+              type="date"
+              name="date_to"
               value={dateFilter?.date_to}
-              color='success'
-              size='small'
+              color="success"
+              size="small"
               onChange={handleDateFilterChange}
             />
           </div>
           <CustomButton
-            color='success'
-            size='small'
+            color="success"
+            size="small"
             rounded
             onClick={searchWithFilters}
             loading={paymentsLoading || usersLoading}
@@ -238,109 +227,106 @@ const Payments = () => {
             Искать...
           </CustomButton>
         </div>
-        <div className='users-list-container'>
-          <table
-            className='users-list'
-            ref={usersListRef}
-          >
+        <div className="users-list-container">
+          <table className="users-list" ref={usersListRef}>
             <thead>
-            <tr>
-              <th>
-                <input
-                  type='checkbox'
-                  checked={chosenPayments?.length === payments?.length}
-                  onChange={(e) => onChoosePayment(e, 'all')}
-                />
-              </th>
-              <th>ЛС абонента</th>
-              <th>СИ</th>
-              <th>Логин</th>
-              <th>Дата оплаты</th>
-              <th>Дата принятия оплаты</th>
-              <th>Баланс</th>
-              <th>Статус оплаты</th>
-              <th>Действия</th>
-            </tr>
-            </thead>
-            <tbody>
-            {payments?.map((payment, i) => (
-              <tr key={i}>
+              <tr>
                 <th>
                   <input
-                    type='checkbox'
-                    onChange={(e) =>
-                      onChoosePayment(e, payment?.number_payment)
-                    }
-                    checked={chosenPayments.includes(payment?.number_payment)}
+                    type="checkbox"
+                    checked={chosenPayments?.length === payments?.length}
+                    onChange={(e) => onChoosePayment(e, 'all')}
                   />
                 </th>
-                <td>{payment.ls_abon || '-'}</td>
-                <td>{payment.user_name || '-'}</td>
-                <td>{payment.login || '-'}</td>
-                <td>
-                  {!!payment.date_payment
-                    ? formatDate(payment.accept_payment)
-                    : '-'}
-                </td>
-                <td>
-                  {!!payment.accept_payment
-                    ? formatDate(payment.accept_payment)
-                    : '-'}
-                </td>
-                <td style={{ textAlign: 'center' }}>
-                  {payment.money || 0}
-                  <span className='currence-highlight'>с</span>
-                </td>
-                <td style={{ textAlign: 'center' }}>
-                  {payment.status_payment || '-'}
-                </td>
-                {['admin'].includes(role) && (
-                  <td>
-                    <div
-                      className='user-action-btns'
-                      style={{ position: 'unset' }}
-                    >
-                      <CustomButton
-                        color='success'
-                        onClick={(_) => onPaymentAnnulment(payment?.id)}
-                        size='small'
-                        loading={paymentInAnnulmentProcess.includes(
-                          payment?.id,
-                        )}
-                      >
-                        аннулировать
-                      </CustomButton>
-                    </div>
-                  </td>
-                )}
+                <th>ЛС абонента</th>
+                <th>СИ</th>
+                <th>Логин</th>
+                <th>Дата оплаты</th>
+                <th>Дата принятия оплаты</th>
+                <th>Баланс</th>
+                <th>Статус оплаты</th>
+                <th>Действия</th>
               </tr>
-            ))}
+            </thead>
+            <tbody>
+              {payments?.map((payment, i) => (
+                <tr key={i}>
+                  <th>
+                    <input
+                      type="checkbox"
+                      onChange={(e) =>
+                        onChoosePayment(e, payment?.number_payment)
+                      }
+                      checked={chosenPayments.includes(payment?.number_payment)}
+                    />
+                  </th>
+                  <td>{payment.ls_abon || '-'}</td>
+                  <td>{payment.user_name || '-'}</td>
+                  <td>{payment.login || '-'}</td>
+                  <td>
+                    {!!payment.date_payment
+                      ? formatDate(payment.accept_payment)
+                      : '-'}
+                  </td>
+                  <td>
+                    {!!payment.accept_payment
+                      ? formatDate(payment.accept_payment)
+                      : '-'}
+                  </td>
+                  <td style={{ textAlign: 'center' }}>
+                    {payment.money || 0}
+                    <span className="currence-highlight">с</span>
+                  </td>
+                  <td style={{ textAlign: 'center' }}>
+                    {payment.status_payment || '-'}
+                  </td>
+                  {['admin'].includes(role) && (
+                    <td>
+                      <div
+                        className="user-action-btns"
+                        style={{ position: 'unset' }}
+                      >
+                        <CustomButton
+                          color="success"
+                          onClick={(_) => onPaymentAnnulment(payment?.id)}
+                          size="small"
+                          loading={paymentInAnnulmentProcess.includes(
+                            payment?.id
+                          )}
+                        >
+                          аннулировать
+                        </CustomButton>
+                      </div>
+                    </td>
+                  )}
+                </tr>
+              ))}
             </tbody>
           </table>
-          <div className='pagination-container'>
-            <div className='list-actions'>
+          <div className="pagination-container">
+            <div className="list-actions">
               <div
-                className='pagination-field-wrapper'
+                className="pagination-field-wrapper"
                 style={{ marginLeft: 'auto' }}
               >
-                <span className='pagination-field-title'>
+                <span className="pagination-field-title">
                   Выберите действие:
                 </span>
                 <Select
-                  color='success'
-                  size='small'
+                  color="success"
+                  size="small"
                   onChange={onListActionChange}
                 >
-                  <option value=''>-</option>
-                  <option value='uploadChosenOptions'>
+                  <option value="">-</option>
+                  <option value="uploadChosenOptions">
                     Выгрузить платежи (старая версия)
                   </option>
-                  <option value='newVersionUpload'>Выгрузить платежи</option>
+                  <option value="newVersionUpload">Выгрузить платежи</option>
                 </Select>
               </div>
               <CustomButton
-                color='success'
-                size='small'
+                color="success"
+                size="small"
                 style={{ marginTop: 'auto' }}
                 rounded
                 onClick={onActionExecute}
@@ -350,12 +336,12 @@ const Payments = () => {
               </CustomButton>
             </div>
             <div
-              className='pagination-field-wrapper'
+              className="pagination-field-wrapper"
               style={{ marginLeft: 'auto' }}
             >
               <CustomButton
-                color='success'
-                size='small'
+                color="success"
+                size="small"
                 style={{ marginTop: 'auto' }}
                 rounded
                 onClick={onShowMore}
